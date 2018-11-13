49325cf773e750fa02c6de4933eb699149f14311
Sai Bhargav Yalamanchi <syalaman@latedays.andrew.cmu.edu>
2018-11-12 23:45:46 -0500

    state class implementation

--- engine/IState.h
+++ engine/IState.h
@@ -10,41 +10,124 @@
 
 #include <vector>
 #include <string>
+#include <list>
+#include "chessboard.h"
+#inculde "chessplayer.h"
 
 namespace msa {
 namespace mcts {
 
 class Action {
+    public:
+        // TODO(sai): verify constructor
+        Action(Move &regular, list<Move> &nulls):
+            regular(regular),
+            nulls(nulls)
+        {
+        }
 
+        Move regular;
+        list<Move> nulls;
 };
 
 class State {
 
-    // copy and assignment operators should perform a DEEP clone of the given state
-    State(const State& other);
-    State& operator = (const State& other);
+    public:
+        State(int depth, int is_white, ChessBoard board):
+            depth(depth),
+            is_white(is_white),
+            board(board)
+        {
+        }
+        // copy and assignment operators should perform a DEEP clone of the given state
+        // TODO(sai): verify deep copy for struct members (particularly arrays)
+        // will default constructors do?
+        //State(const State& other);
+        //State& operator = (const State& other);
 
-    // whether or not this state is terminal (reached end)
-    bool is_terminal() const;
+        // whether or not this state is terminal (reached end)
+        bool is_terminal() {
+            // for mate in n puzzles, we do not want to look at nodes deeper than n
+            if (depth == 0)
+                return true;
 
-    //  agent id (zero-based) for agent who is about to make a decision
-    int agent_id() const;
+            int color = get_color(); 
+            ChessPlayer::Status status = board.getPlayerStatus(color);
+            if (status == ChessPlayer::Checkmate || status == ChessPlayer::Stalemate)
+                return true;
+            return false;
+        }
 
-    // apply action to state
-    void apply_action(const Action& action);
+        //  agent id (zero-based) for agent who is about to make a decision
+        int agent_id() {
+            return is_white;
+        } 
 
-    // return possible actions from this state
-    void get_actions(std::vector<Action>& actions) const;
+        // apply action to state
+        void apply_action(const Action& action) {
+            // toggle player color
+            is_white = !is_white;
+            // if it's white's turn, decrease depth value 
+            if (is_white) depth--;
+            
+            board.move(action.regular);
+            for(list<Move>::iterator it=action.nulls.begin(); it!=action.nulls.end(); it++)
+                board.move(*it);
+        }
 
-    // get a random action, return false if no actions found
-    bool get_random_action(Action& action) const;
+        // return possible actions from this state
+        void get_actions(std::vector<Action>& actions) {
+            // sanity check
+            if (is_terminal()) return;            
 
-    // evaluate this state and return a vector of rewards (for each agent)
-    const std::vector<float> evaluate() const;
+            list<Move> regulars, nulls;
+            // TODO(sai): use shared pointers for nulls
+            board.getMoves(get_color(), regulars, regulars, nulls);
+            for(list<Move>::iterator it=regulars.begin(); it!=regulars.end(); it++)
+                actions.push_back(Action(*it, nulls));
+        }
 
-    // return state as string (for debug purposes)
-    std::string to_string() const;
+        // get a random action, return false if no actions found
+        bool get_random_action(Action& action) {
+            if (is_terminal()) return false;
 
+            std::vector<Action>& actions;
+            get_actions(actions);
+            std::default_random_engine generator;
+            std::uniform_int_distribution<int> dis(0, actions.size()-1);
+            action = actions[dis(generator)];
+
+            return true;
+        }
+
+        // evaluate this state and return a vector of rewards (for each agent)
+        const std::vector<float> evaluate() {
+            // TODO(sai): implement logic for 2 player AI
+            // sanity check
+            if (!is_white) return;
+
+            vector<float> victory{0, 1};
+            vector<float> loss{1, 0};
+            if (depth < 0) return loss;
+
+            ChessPlayer::Status status = board.getPlayerStatus(color);
+            if (status == ChessPlayer::Checkmate)
+                return victory;            
+            
+            return loss;
+        } 
+
+        // return state as string (for debug purposes)
+        std::string to_string() const;
+
+        int get_color() {
+            return is_white ? WHITE : BLACK;
+        }
+
+        // the root state's depth is initialized with n (mate in n)
+        int depth;
+        int is_white;
+        ChessBoard board;
 };
 
 }
